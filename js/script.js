d2d3af54df2ff16fe49900889880b659a1f01a13
Mateusz <mat.sek@wp.pl>
2018-02-06 18:23:52 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var $nytHeaderElem = $('#nytimes-header'); //some variables regarding NY Times to store the heading and article itself
-var $nytElem = $('#nytimes-articles');
+var $nytHeaderElem; //some variables regarding NY Times to store the heading and article itself
+var $nytElem;
 
 function NYTSearch(){  // NY Times search function
     var nyItem = $("#nyItem").val();
@@ -28,28 +28,20 @@ function initMap(latit,longit) { // Google Maps display function
     });
 }
 
-cities = ['krakow','wroclaw','warszawa','gdansk','california'] //to add new city, put data into this 3 tables
-lat_data = [50.0646501,51.107885,52.229676,54.352025,33.764000]
-long_data = [19.9449799,17.038538,21.012229,18.646638,-118.182356]
-
 var main = function(){
+    $nytHeaderElem = $('#nytimes-header'); //some variables regarding NY Times to store the heading and article itself
+    $nytElem = $('#nytimes-articles');
 
     initMap(51.509865,-0.118092) //starting map, for example London
 
     $("#showMap").on('click',function(){ //showing google map once "Pokaż" button is clicked
-        lat_var = parseInt($('#latitude').val()*1000000)/1000000; //multiplying-dividing to have the precision of coordinates
-        long_var = parseInt($('#longitude').val()*1000000)/1000000;
-        initMap(lat_var,long_var);
+        data_lat = parseInt($('#latitude').val()*1000000)/1000000; //multiplying-dividing to have the precision of coordinates
+        data_long = parseInt($('#longitude').val()*1000000)/1000000;
+        initMap(data_lat,data_long);
     });
 
     $(".city").on('click', function(){
-        for (let i=0;i<cities.length;i++){
-            if ($(this).attr("id")==cities[i]){
-                lat_var = lat_data[i];
-                long_var = long_data[i]
-            }
-        }
-        initMap(lat_var,long_var);
+        initMap((parseInt($(this).attr("data-lat")*1000000)/1000000),(parseInt($(this).attr("data-long")*1000000)/1000000));
     })
 
     $("#showArticles").on('click',NYTSearch); //NYTimes search button
